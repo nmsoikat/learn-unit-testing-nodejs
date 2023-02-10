98743a53a399239946386a7e6802e98e96e6f88a
nmsoikat <nursaykat@gmail.com>
2023-02-10 16:08:00 +0600

    utils

--- module3-testing-the-app/lib/utils.test.js
+++ module3-testing-the-app/lib/utils.test.js
@@ -10,34 +10,33 @@ var crypto = require('crypto');
 
 var config = require('./config');
 var utils = require('./utils');
-var sandbox = sinon.sandbox.create();
+var sandbox = sinon.createSandbox()
 
-describe('utils', ()=>{
-    let secretStub, digestStub, updateStub, createHashStub, hash;
+describe('utils', () => {
+    let secretStub, createHashStub, hash, updateStub, digestStub;
 
-    beforeEach(()=>{
-        secretStub = sandbox.stub(config, 'secret').returns('fake_secret');
-        digestStub = sandbox.stub().returns('ABC123');
+    beforeEach(() => {
+        secretStub = sandbox.stub(config, 'secret').returns("fake_secret")
+        digestStub = sandbox.stub().returns('ABC123')
         updateStub = sandbox.stub().returns({
             digest: digestStub
-        });
+        })
         createHashStub = sandbox.stub(crypto, 'createHash').returns({
             update: updateStub
-        });
-
-        hash = utils.getHash('foo');
+        })
+        hash = utils.getHash('foo')
     })
 
-    afterEach(()=>{
-        sandbox.restore();
+    afterEach(() => {
+        sandbox.restore()
     })
 
-    it('should return null if invalid string is passed', ()=>{
-        sandbox.reset();
+    it("should return null if invalid staring is pass", async () => {
+        sandbox.reset()
 
         let hash2 = utils.getHash(null);
         let hash3 = utils.getHash(123);
-        let hash4 = utils.getHash({name: 'bar'});
+        let hash4 = utils.getHash({ bar: "test" });
 
         expect(hash2).to.be.null;
         expect(hash3).to.be.null;
@@ -46,14 +45,15 @@ describe('utils', ()=>{
         expect(createHashStub).to.not.have.been.called;
     })
 
-    it('should get secret from config', ()=>{
+    it("should get secret from config", async () => {
         expect(secretStub).to.have.been.called;
+        expect(secretStub()).to.equal("fake_secret")
     })
 
-    it('should call crypto with correct settings and return hash', ()=>{
+    it("should call crypto with correct settings and return hash", () => {
         expect(createHashStub).to.have.been.calledWith('md5');
         expect(updateStub).to.have.been.calledWith('foo_fake_secret');
         expect(digestStub).to.have.been.calledWith('hex');
-        expect(hash).to.equal('ABC123');
+        expect(hash).to.equal('ABC123')
     })
 })
