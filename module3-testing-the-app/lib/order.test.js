1c04069c8114354dadeda04cb156183f78a415ee
nmsoikat <nursaykat@gmail.com>
2023-05-04 23:47:39 +0600

    test class

--- module3-testing-the-app/lib/order.test.js
+++ module3-testing-the-app/lib/order.test.js
@@ -1,76 +1,118 @@
-const chai = require('chai');
+const chai = require('chai')
 const expect = chai.expect;
-const sinon = require('sinon');
-const sinonChai = require('sinon-chai');
-chai.use(sinonChai);
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
 
-var Order = require('./order');
-var sandbox = sinon.sandbox.create();
+chai.use(sinonChai)
 
-describe('order', ()=>{
-    let warnStub, dateSpy, user, items, o;
+const Order = require('./order')
+const sandbox = sinon.createSandbox()
 
-    beforeEach(()=>{
+describe('Order Class', () => {
+    let warnStub, dateSpy, user, items, order;
+    beforeEach(() => {
         warnStub = sandbox.stub(console, 'warn');
         dateSpy = sandbox.spy(Date, 'now');
-
-        user = {id: 1, name: 'foo'};
+        user = { id: 1, name: 'foo' };
         items = [
-            {name:'Book', price: 10},
-            {name:'Dice set', price: 5}
-        ]
+            { name: 'Book', price: 10 },
+            { name: 'Dice set', price: 5 }
+        ];
 
-        o = new Order(123, user, items);
+        order = new Order(123, user, items);
     })
 
-    afterEach(()=>{
+    afterEach(() => {
         sandbox.restore();
     })
 
-    it('should create instance or Order and calculate total + shipping', ()=>{
-        expect(o).to.be.instanceOf(Order);
+
+    it("should create instance or Order and calculate total + shipping", () => {
+        expect(order).to.be.instanceof(Order);
         expect(dateSpy).to.have.been.calledTwice;
-        expect(o).to.have.property('ref').to.equal(123);
-        expect(o).to.have.property('user').to.deep.equal(user);
-        expect(o).to.have.property('items').to.deep.equal(items);
-        expect(o).to.have.property('status').to.equal('Pending');
-        expect(o).to.have.property('createdAt').to.be.a('Number');
-        expect(o).to.have.property('updatedAt').to.be.a('Number');
-        expect(o).to.have.property('subtotal').to.equal(15);
-        expect(o).to.have.property('shipping').to.equal(5);
-        expect(o).to.have.property('total').to.equal(20);
+        expect(order)
+            .to.have.property('ref')
+            .to.equal(123);
+        expect(order)
+            .to.have.property('user')
+            .to.deep.equal(user);
+        expect(order)
+            .to.have.property('items')
+            .to.deep.equal(items);
+        expect(order)
+            .to.have.property('status')
+            .to.equal('Pending');
+        expect(order)
+            .to.have.property('createdAt')
+            .to.be.a('Number');
+        expect(order)
+            .to.have.property('updatedAt')
+            .to.be.a('Number');
+        expect(order)
+            .to.have.property('subtotal')
+            .to.be.a('Number')
+            .to.be.equal(15);
+        expect(order)
+            .to.have.property('shipping')
+            .to.equal(5);
+        expect(order)
+            .to.have.property('total')
+            .to.equal(20)
 
-        expect(o.save).to.be.a('function');
-        expect(o.cancel).to.be.a('function');
-        expect(o.ship).to.be.a('function');
+        expect(order.save).to.be.a('function')
+        expect(order.cancel).to.be.a('function')
+        expect(order.ship).to.be.a('function')
     })
 
-    it('should update status to active and return order details', ()=>{
-        let result = o.save();
+    it('should update status to active and return order details', () => {
+        const result = order.save();
 
+        expect(dateSpy).to.have.calledThrice;
+        expect(order)
+            .to.have.property('status')
+            .to.equal('Active')
+        expect(order)
+            .to.have.property('updatedAt')
+            .to.be.a('Number')
+
+        expect(result).to.be.a('Object')
+        expect(result)
+            .to.have.property('user')
+            .to.equal('foo')
+    })
+
+    it('Should cancel an order, update status and set shipping and total to zero', () => {
+        const result = order.cancel()
         expect(dateSpy).to.have.been.calledThrice;
-        expect(o.status).to.equal('Active');
-        expect(result).to.be.a('Object');
-        expect(result).to.have.property('user').to.equal('foo');
-        expect(result).to.have.property('updatedAt').to.be.a('Number');
-        //rest of props
-    })
 
-    it('should cancel an order, update status and set shipping and total to zero', ()=>{
-        let result = o.cancel();
+        expect(order)
+            .to.have.property('status')
+            .to.equal('Cancelled')
+        expect(order)
+            .to.have.property('updatedAt')
+            .to.be.a('Number')
+        expect(order)
+            .to.have.property('shipping')
+            .to.equal(0)
+        expect(order)
+            .to.have.property('total')
+            .to.equal(0)
 
-        expect(warnStub).to.have.been.calledWith('Order cancelled');
-        expect(dateSpy).to.have.been.calledThrice;
-        expect(o.status).to.equal('Cancelled');
-        expect(result).to.be.true;
-        expect(o.shipping).to.equal(0);
-        expect(o.total).to.equal(0);
-    })
+        expect(warnStub).to.have.been.calledWith("Order cancelled")
 
-    it('should update status to shipped', ()=>{
-        o.ship();
+        expect(result).to.be.true
+    });
 
-        expect(o.status).to.equal('Shipped');
-        expect(dateSpy).to.have.been.calledThrice;
-    })
+    it('Should update status to shipped', () => {
+        order.ship()
+
+        expect(dateSpy).to.have.been.calledThrice
+
+        expect(order)
+            .to.have.property('status')
+            .to.equal('Shipped')
+        expect(order)
+            .to.have.property('updatedAt')
+            .to.be.a('Number')
+    });
 })
